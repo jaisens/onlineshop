334651c72aa8c862d665fb2973ec6fd84ac2251e
Jai Kumar Verma <vermajai2001@gmail.com>
2023-11-16 11:34:44 +0530

    first commit

---
+++ frontend/src/components/admin/lists/Productslist.jsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+
+export default function Productslist() {
+  const { items } = useSelector((state) => state.products);
+
+  const rows =
+    items &&
+    items.map((item) => {
+      return {
+        id: item._id,
+        imageUrl: item.image.url,
+        pName: item.name,
+        pDesc: item.desc,
+        price: item.price.toLocaleString(),
+      };
+    });
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 220 },
+    {
+      field: "imageUrl",
+      headerName: "Image",
+      width: 80,
+      renderCell: (params) => {
+        return <div>img</div>;
+      },
+    },
+    { field: "pName", headerName: "Name", width: 130 },
+    {
+      field: "pDesc",
+      headerName: "Description",
+      width: 130,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 80,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      sortable: false,
+      width: 170,
+      valueGetter: (params) =>
+        `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    },
+  ];
+  return (
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  );
+}
